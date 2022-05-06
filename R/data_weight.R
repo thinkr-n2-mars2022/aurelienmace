b505dac6f01cab3b65b4bf9926d4e5224e541a69
amaceFIND <aurelien.mace@finddx.org>
2022-05-06 11:58:42 +0200

    add data weight dataset

---
+++ R/data_weight.R
@@ -0,0 +1,12 @@
+#' Weight and height of Star War characters
+#'
+#' A dataset #' Weight and height of Star War characters
+#'
+#' @format A data frame with 87 rows and 3 variables:
+#' \describe{
+#' \item{name}{name of character}
+#' \item{height}{height in meter}
+#' \item{mass}{mass in kg}
+#' ...
+#' }
+"data_weight"
